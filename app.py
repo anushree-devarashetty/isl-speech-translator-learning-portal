285929d1540c390e686a3cb279e2be598d96d00e
Anushree Shetty <anushreeshetty@Anushrees-MacBook-Pro.local>
2026-03-12 14:20:59 +0530

    deploy isl translator

--- app.py
+++ app.py
@@ -478,5 +478,6 @@ def video_conversion(lema_isl_sent_list):
 
 # ---------------------------------------------------
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, port=5001)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
